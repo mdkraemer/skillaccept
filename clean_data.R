055bec21525dc8eaf91bd49a63c2037c86108117
Michael D. Krämer <mkraemer@diw.de>
2023-07-05 16:20:18 +0200

    first push

---
+++ clean_data.R
@@ -0,0 +1,11 @@
+# Script for Data Cleaning
+
+library(renv)
+library(tidyverse)
+
+### Package / dependency version management with 'renv'
+# only ran this to initialize 'renv'
+# renv::init()
+# renv::snapshot()
+# renv::restore() # to revert to the previous state as encoded in the lockfile
+
